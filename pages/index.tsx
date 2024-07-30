91ccaacfff92fec168200a3139740915d7d195d8
김태관 <iamtk@neowiz.com>
2024-07-30 09:24:17 +0900

    dynamic routing 작성중

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,19 +23,21 @@ export function getStaticProps() {
 export default function Home({ allPostsData }: Props) {
   return (
     <Layout home>
-      <h2 className="headingXl">Blog</h2>
-      <ul>
-        {allPostsData.map(({ id, title, date }) => (
-          <li key={id}>
-            {title}
-            <br />
-            {id}
-            <br />
-            {date}
-            <br />
-          </li>
-        ))}
-      </ul>
+      <section className="headingMd pt-[1px]">
+        <h2 className="headingLg">Blog</h2>
+        <ul className="list-none p-0 m-0">
+          {allPostsData.map(({ id, title, date }) => (
+            <li className="mt-0 mx-0 mb-5" key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+              <br />
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,6 @@
+import { useRouter } from "next/router";
+
+const Post = () => {
+  return <div>Post</div>;
+};
+export default Post;
