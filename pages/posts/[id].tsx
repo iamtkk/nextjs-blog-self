2adec5c32fc734ba958955673c384372f5bfd5ad
김태관 <iamtk@neowiz.com>
2024-07-31 23:46:51 +0900

    getPostData 작성중

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,24 @@
 import { useRouter } from "next/router";
+import { getAllPostIds, getPostData } from "@/lib/posts.mjs";
+
+export function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    params: {
+      paths,
+      fallback: true,
+    },
+  };
+}
+
+export function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
 
 const Post = () => {
   return <div>Post</div>;
